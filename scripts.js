5b6889bb0af6d871c21919757e923f9547397ae4
Lucía <dellam.lucia@outlook.com>
2025-01-07 21:53:53 -0300

    cambios
    intento de menu y carousel fallidos

--- scripts.js
+++ scripts.js
@@ -50,3 +50,38 @@ document.querySelectorAll('.menu-item p').forEach(item => {
         }
     });
 });
+
+
+
+
+//carousel
+
+const carousel = document.querySelector('.carousel');
+const rightBtn = document.getElementById('right-btn');
+
+//numero d cajas visibles
+const boxesPerView = 3;
+
+//funcion para mover el carousel a la derecha
+rightBtn.addEventListener('click', () => {
+    // Desplazar el carrousel hacia la izquierda en base al número de cajas visibles
+    const currentTransform = carousel.style.transform ? parseInt(carousel.style.transform.replace('translateX(', '').replace('px)', '')) : 0;
+    const maxTransform = -(carousel.scrollWidth - carousel.clientWidth); // Límite de desplazamiento a la izquierda
+
+    if (currentTransform > maxTransform) {
+        carousel.style.transform = `translateX(${currentTransform - boxWidth * boxesPerView}px)`;
+    }
+});
+
+
+const element = document.getElementById('miElemento');
+if (element) {
+    element.addEventListener('click', miFuncion);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const element = document.getElementById('miElemento');
+    if (element) {
+        element.addEventListener('click', miFuncion);
+    }
+});
